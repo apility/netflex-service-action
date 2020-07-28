f4d4650625c040573de60eebe5fe2c1f6f889cfe
Tommy Stigen Olsen <tommysolsen@gmail.com>
2020-07-28 10:56:52 +0200

    Dont concat string and MatchData

--- action.rb
+++ action.rb
@@ -37,12 +37,12 @@ unless options.fetch(:CREATE, false) or options.fetch(:TEARDOWN, false)
   exit
 end
 
-PR_TAG = "pr-" + /refs\/pull\/(\d+)\/merge/.match(options.fetch(:PR_TAG))
+PR_TAG = /refs\/pull\/(\d+)\/merge/.match(options.fetch(:PR_TAG))
 unless PR_TAG
   puts "Unable to extract pull request number"
   exit
 end
-PR_TAG = PR_TAG[1]
+PR_TAG = "pr-" + PR_TAG[1]
 
 REPOSITORY = options.fetch(:REPOSITORY).downcase
 SITE_NAME = /\/(.*)$/.match(REPOSITORY)[1]
